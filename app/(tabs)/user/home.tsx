f4ba359cc20cd1e66b79b32252d966e615c0df1a
Nashir Jamali <nashirjamali@gmail.com>
2025-05-25 04:31:52 +0700

    add token card parallax

--- app/(tabs)/user/home.tsx
+++ app/(tabs)/user/home.tsx
@@ -2,10 +2,13 @@ import React from 'react';
 
 import { View, Text } from 'react-native';
 
+import TokenBalance from '@/components/ui/TokenBalance';
+import TokenBalanceCarousel from '@/components/ui/TokenBalanceCarousel';
+
 const UserHome = () => {
   return (
     <View>
-      <Text>UserHome</Text>
+      <TokenBalanceCarousel />
     </View>
   );
 };
