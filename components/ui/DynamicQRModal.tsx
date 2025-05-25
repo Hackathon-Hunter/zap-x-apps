d0aa1c8877b420447dbc959472c549c18d445671
denandrapr <denandra1628@gmail.com>
2025-05-25 23:26:07 +0700

    feat: add dynamic QR modal --no-verify

---
+++ components/ui/DynamicQRModal.tsx
@@ -0,0 +1,166 @@
+import React, { useEffect, useRef } from 'react';
+
+import {
+  Image,
+  StyleSheet,
+  ToastAndroid,
+  TouchableOpacity,
+  View,
+} from 'react-native';
+
+import QRCode from 'react-native-qrcode-svg';
+import Share from 'react-native-share';
+
+import { Colors } from '@/constants/Colors';
+
+import Modal from './ThemedModal';
+import DownloadFileIcon from '../icons/DownloadIcon';
+import ShareIcon from '../icons/ShareIcon';
+import ThemeButton from '../ThemedButton';
+import { ThemedText } from '../ThemedText';
+
+interface PaymentSuccessModalProps {
+  visible: boolean;
+  onClose: () => void;
+  qrData: {
+    url: string;
+    ammount: string;
+  };
+  onShareReceipt?: () => void;
+  onDownloadReceipt?: () => void;
+}
+
+type QRCodeRef = {
+  toDataURL: (callback: (data: string) => void) => void;
+};
+
+const DynamicQRModal: React.FC<PaymentSuccessModalProps> = ({
+  visible,
+  onClose,
+  qrData,
+  onShareReceipt,
+  onDownloadReceipt,
+}: {
+  visible: boolean;
+  onClose: () => void;
+  qrData: {
+    url: string;
+    ammount: string;
+  };
+  onShareReceipt: () => void;
+  onDownloadReceipt: () => void;
+}) => {
+  const qrRef = useRef<QRCodeRef>(null);
+
+  const onShareReceiptt = async () => {
+    if (!qrRef.current) {
+      ToastAndroid.show('QR Code not generated yet!', ToastAndroid.SHORT);
+      return;
+    }
+
+    qrRef.current.toDataURL(async (dataURL) => {
+      const base64Image = `data:image/png;base64,${dataURL}`;
+      await shareQR(base64Image);
+    });
+  };
+
+  const shareQR = async (base64Image: string) => {
+    try {
+      const shareOptions = {
+        title: 'Share QR Code',
+        message: 'Check out this payment QR code',
+        url: base64Image,
+        type: 'image/png',
+      };
+      await Share.open(shareOptions);
+    } catch (error) {
+      // Handle error if needed
+    }
+  };
+
+  return (
+    <Modal
+      visible={visible}
+      onClose={onClose}
+      showCloseButton={true}
+      title="Dynamic QR"
+    >
+      <View className="items-center">
+        <View style={styles.qrContainer}>
+          <QRCode
+            value={qrData.url}
+            getRef={(c) => (qrRef.current = c)}
+            size={200}
+          />
+          <View style={styles.logoContainer}>
+            <Image
+              source={require('@/assets/images/zapx-logo.png')}
+              style={styles.logo}
+            />
+          </View>
+        </View>
+        <View className="flex flex-row items-end gap-2 mt-4">
+          <ThemedText
+            color={Colors.dark.text.secondary}
+            numbersOnly
+            className="text-base font-medium"
+          >
+            IDR
+          </ThemedText>
+          <ThemedText
+            color={Colors.dark.text.primary}
+            numbersOnly
+            className="text-4xl font-medium mt-1"
+            style={{ textShadowColor: 'white', textShadowRadius: 10 }}
+          >
+            {qrData.ammount}
+          </ThemedText>
+        </View>
+        <View className="flex flex-row items-end gap-2 mt-4">
+          <ThemeButton
+            variant="primary"
+            onPress={onShareReceiptt}
+            text="Share"
+            LeftIcon={ShareIcon}
+          />
+        </View>
+        <View className="flex-row justify-center items-center py-4 bg-black">
+          <ThemedText color={Colors.dark.text.primary}>─────────</ThemedText>
+          <ThemedText color={Colors.dark.text.primary}> OR </ThemedText>
+          <ThemedText color={Colors.dark.text.primary}>─────────</ThemedText>
+        </View>
+        <TouchableOpacity
+          onPress={onDownloadReceipt}
+          activeOpacity={0.7}
+          className="flex-row justify-center items-center bg-black gap-2 p-2 rounded"
+        >
+          <DownloadFileIcon />
+          <ThemedText color={Colors.dark.text.primary}>Download</ThemedText>
+        </TouchableOpacity>
+      </View>
+    </Modal>
+  );
+};
+
+export default DynamicQRModal;
+
+const styles = StyleSheet.create({
+  qrContainer: {
+    position: 'relative',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  logoContainer: {
+    position: 'absolute',
+    backgroundColor: 'transparent',
+    borderRadius: 25,
+    width: 60,
+    height: 60,
+    overflow: 'hidden',
+  },
+  logo: {
+    width: '100%',
+    height: '100%',
+    resizeMode: 'contain',
+  },
+});
